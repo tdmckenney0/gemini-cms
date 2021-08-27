c777f0268fb78ae8c309983f9aa22274a2db4220
Tanner Mckenney <tmckenney7@outlook.com>
2021-08-26 22:25:47 -0700

    Add title

--- db/schema.sql
+++ db/schema.sql
@@ -3,11 +3,12 @@ BEGIN TRANSACTION;
 CREATE TABLE `pages` (
     `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     `slug` VARCHAR(128), 
+    `title` VARCHAR(255),
     `created_at` DATETIME NOT NULL default current_timestamp, 
     `updated_at` DATETIME NOT NULL default current_timestamp, 
     `body` TEXT NOT NULL
 );
-INSERT INTO pages VALUES(1,'welcome','2021-08-27 04:40:00','2021-08-27 04:40:00','Hello World!');
+INSERT INTO pages VALUES(1,'welcome','Welcome!!','2021-08-27 04:40:00','2021-08-27 04:40:00','Hello World!');
 DELETE FROM sqlite_sequence;
 INSERT INTO sqlite_sequence VALUES('pages',1);
 COMMIT;
